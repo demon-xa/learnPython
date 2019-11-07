d4b895697e14f77b96c82e80b8ee7911adfb9e87
demon-xa <demon-xa@ya.ru>
2019-11-07 15:17:37 +0300

    11

--- test22.py
+++ test22.py
@@ -1,2 +1,36 @@
-print ("Hello")
+def is_even(x):
+    if x == 0:
+        return True
+    else:
+        return is_odd(x-1)
 
+def is_odd(x):
+    return not is_even(x)
+
+
+print(is_odd(3))
+print(is_even(4))
+
+class Cat:
+  def __init__(self, color, legs):
+    self.color = color
+    self.legs = legs
+
+felix = Cat("ginger", 4)
+rover = Cat("dog-colored", 4)
+stumpy = Cat("brown", 3)
+
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __gt__(self, other):
+        for index in range(len(other.cont)+1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+
+spam = SpecialString("spam")
+eggs = SpecialString("eggs")
+spam > eggs
--- test3.py
+++ test3.py
@@ -1,3 +1,12 @@
-#comment
-#comment2
-#comment3
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+
+spam = SpecialString("spam")
+test = SpecialString("HHHHHflekwhkfdjg")
+hello = SpecialString("Hello world!")
+print(spam / hello +'\n'+ test / hello )
